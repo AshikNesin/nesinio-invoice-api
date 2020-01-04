df4671e46f42f1864b036d06395543c90e03d1f3
Ashik <mail@ashiknesin.com>
2020-01-05 02:57:22 +0530

    Add updateInvoice mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -19,6 +19,33 @@ const Mutations = {
         console.log(JSON.stringify(invoice, null, 2));
         const doc = await Invoice.create(invoice);
         return doc;
+    },
+    async updateInvoice({ data }) {
+        const updates = {
+            ...data,
+            lineItems: data.lineItems.map(item => {
+                if (!item.id) {
+                    item.id = mongoose.Types.ObjectId();
+                }
+                return {
+                    ...item
+                };
+            })
+        };
+
+        const updatedDoc = await Invoice.findOneAndUpdate(
+            { _id: data.id },
+            { $set: updates },
+            { new: true, runValidators: true, context: 'query' }
+        );
+
+        console.log(JSON.stringify(data, null, 2));
+        console.log(JSON.stringify(updates, null, 2));
+
+        console.log(`to update`);
+
+        console.log(JSON.stringify(updatedDoc, null, 2));
+        return updatedDoc;
     }
 };
 
--- src/schema.js
+++ src/schema.js
@@ -43,15 +43,28 @@ input LineItemInput {
     unitAmount: Int!
     quantity:Int!
 }
+input LineItemEditInput {
+  id:ID
+  description: String!
+  unitAmount: Int!
+  quantity:Int!
+}
 
 input CreateInvoiceInput {
     customerId: ID!
     lineItems:[LineItemInput]
 }
 
+input UpdateInvoiceInput {
+  id: ID!
+  lineItems:[LineItemEditInput]
+}
+
+
 type Mutation {
     createCustomer(name: String!, email: String, mobile: Int, address:String ): Customer!
     createInvoice(data:CreateInvoiceInput): Invoice!
+    updateInvoice(data:UpdateInvoiceInput): Invoice!
 }
 `);
 
