0645c188c8d0dc4c9fffcec4fc240c45b5c81f6f
Ashik <mail@ashiknesin.com>
2020-01-05 10:39:33 +0530

    Move controllers to Mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 const Invoice = mongoose.model('Invoice');
 const Customer = mongoose.model('Customer');
-// const uuidv4 = require('uuid/v4');
 
 const Mutations = {
-    async createInvoice(customer) {
+    async createCustomer(customer) {
         const doc = await Customer.create(customer);
         return doc;
     },
     async createInvoice({ data }) {
+        const customer = await Customer.findById(data.customerId);
+        if (!customer) {
+            throw new Error('Customer does not exists');
+        }
         const invoice = {
             ...data,
             lineItems: data.lineItems.map(item => ({
@@ -16,7 +19,6 @@ const Mutations = {
                 ...item
             }))
         };
-        console.log(JSON.stringify(invoice, null, 2));
         const doc = await Invoice.create(invoice);
         return doc;
     },
@@ -38,13 +40,18 @@ const Mutations = {
             { $set: updates },
             { new: true, runValidators: true, context: 'query' }
         );
+        return updatedDoc;
+    },
+    async updateCustomer({ data }) {
+        const updates = {
+            ...data
+        };
 
-        console.log(JSON.stringify(data, null, 2));
-        console.log(JSON.stringify(updates, null, 2));
-
-        console.log(`to update`);
-
-        console.log(JSON.stringify(updatedDoc, null, 2));
+        const updatedDoc = await Customer.findOneAndUpdate(
+            { _id: data.id },
+            { $set: updates },
+            { new: true, runValidators: true, context: 'query' }
+        );
         return updatedDoc;
     }
 };
